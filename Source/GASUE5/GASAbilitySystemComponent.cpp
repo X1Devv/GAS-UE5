5750d538ca475625991c1f6792f09b419dcf01b9
vesell <rommal563@gmail.com>
2026-03-14 05:41:29 +0200

    added custom AbilitySystemComponent class

---
+++ Source/GASUE5/GASAbilitySystemComponent.cpp
@@ -0,0 +1,71 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GASAbilitySystemComponent.h"
+
+#include "BaseCharacter.h"
+
+
+// Sets default values for this component's properties
+UGASAbilitySystemComponent::UGASAbilitySystemComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGASAbilitySystemComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+void UGASAbilitySystemComponent::OnRep_ActivateAbilities()
+{
+	Super::OnRep_ActivateAbilities();
+	
+	ABaseCharacter *Character = Cast<ABaseCharacter>(GetOwner());
+	if (!Character) return;
+	
+	
+	bool bAbilitiesChanged = false;
+	
+	if (LastActivatableAbilities.Num() != ActivatableAbilities.Items.Num())
+	{
+		bAbilitiesChanged = true;
+	} else
+	{
+		for (int32  i = 0; i < LastActivatableAbilities.Num(); i++)
+		{
+			if (LastActivatableAbilities[i].Ability != ActivatableAbilities.Items[i].Ability)
+			{
+				bAbilitiesChanged = true;
+				break;
+			}
+		}
+	}
+	if (bAbilitiesChanged)
+	{
+		Character->SendAbilitiesChangedEvent();
+		LastActivatableAbilities = ActivatableAbilities.Items;
+	}
+	
+	
+}
+
+
+// Called every frame
+void UGASAbilitySystemComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                               FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
