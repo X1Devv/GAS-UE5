5b5b8bc9917c09f0f1cb9ec1626424a5842a7747
vesell <rommal563@gmail.com>
2026-03-14 05:39:35 +0200

    added custom gameplayability class

---
+++ Source/GASUE5/Abilities/GASGameplayAbility.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GASGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASUE5_API UGASGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
+	bool ShouldShowInAbilitiesContainer = false;
+};
